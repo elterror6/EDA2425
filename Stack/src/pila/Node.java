efeca94581347712a52e3a93c67a844009b8aa94
Daniel Diaz Garcia <danibasketball2000@gmail.com>
2024-10-02 23:54:23 +0200

    COMMIT: Creación de los métodos para la clase Node

--- Stack/src/pila/Node.java
+++ Stack/src/pila/Node.java
@@ -1,5 +1,63 @@
 package pila;
-
+/**
+ * <h1>Node</h1>
+ * <p>
+ * Esta clase representa una estructura de nodos, de tal manera que se guarde un elemento y el siguiente elemento de la estructura.
+ * </p>
+ * @author Daniel Díaz García
+ */
 public class Node<E> {
-
+    private E element;
+    private Node<E> next;
+    /**
+     * <h2>Node()</h2>
+     * <p>
+     * Método constructor que crea un nuevo nodo con un elemento y sin siguiente.
+     * </p>
+     * @param element Elemento del nodo
+     */
+    public Node(E element) {
+        this.element = element;
+        this.next = null;
+    }
+    /**
+     * <h2>getElement()</h2>
+     * <p>
+     * Método que devuelve el elemento del nodo.
+     * </p>
+     * @return Elemento del nodo
+     */
+    public E getElement() {
+        return element;
+    }
+    /**
+     * <h2>getNext()</h2>
+     * <p>
+     * Método que devuelve el siguiente nodo de la estructura.
+     * </p>
+     * @return Nodo siguiente de la estructura
+     */
+    public Node<E> getNext() {
+        return next;
+    }
+    /**
+     * <h2>setNext()</h2>
+     * <p>
+     * Método que establece el siguiente nodo de la estructura.
+     * </p>
+     * @param next Nodo siguiente de la estructura
+     */
+    public void setNext(Node<E> next) {
+        this.next = next;
+    }
+    /**
+     * <h2>setElement()</h2>
+     * <p>
+     * Método que establece el elemento del nodo.
+     * </p>
+     * @param element Elemento del nodo
+     */
+    public void setElement(E element) {
+        this.element = element;
+    }
 }
