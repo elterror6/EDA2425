5f254eff2837630b61c8a69b86b6190c9f0f8d92
Daniel Diaz Garcia <danibasketball2000@gmail.com>
2024-10-03 00:25:07 +0200

    COMMIT: Documentación y creación de metodos push(), pop() y top().

--- Stack/src/pila/DinamicStack.java
+++ Stack/src/pila/DinamicStack.java
@@ -1,37 +1,89 @@
 package pila;
-
+/**
+ * <h1>DinamicStack</h1>
+ * <p>
+ * Clase que implementa la interfaz Stack para una pila dinámica con una estructura de nodos.
+ * </p>
+ * @author Daniel Díaz García
+ */
 public class DinamicStack<E> implements Stack<E> {
 	private int size;
 	private Node<E> top;
-	
-	 private static class Node<E> {
-	        E data;
-	        Node<E> next;
-
-	        public Node(E data) {
-	            this.data = data;
-	        }
-	    }
-	  public DinamicStack() {
+	/**
+     * <h2>DinamicStack()</h2>
+     * <p>
+     * Método constructor que crea una nueva pila vacía.
+     * </p>
+     */
+	public DinamicStack() {
 	        top = null;
 	        size = 0;
-	    }
-	  
-	public int size() {
-		/**
-		 * cada vez que se hace un push aumenta el size
-		 *  y cada vez que se usa un pop disminuye el size
-		 */
-        return size;
+	}
+    /**
+     * <h2>push()</h2>
+     * <p>
+     * Método que inserta un nuevo elemento en la pila. Se crea un nuevo nodo con el elemento y lo añade al principio de la pila.
+     * </p>
+     * @param element Elemento a insertar en la pila
+     */
+    @Override
+    public void push(E element) {
+        Node<E> aux = new Node<E>(element);
+        aux.setNext(top);
+        top = aux;
+        ++size;
+    }
+    /**
+     * <h2>pop()</h2>
+     * <p>
+     * Método que elimina y devuelve el elemento que está en la cima de la pila. Se comprueba si la pila está vacía, si lo está, se lanza una excepción de tipo EmptyStackException.
+     * </p>
+     * @return Elemento que se ha eliminado de la pila
+     */
+    public E pop() throws EmptyStackException {
+        if (isEmpty()) {
+            throw new EmptyStackException("La pila está vacía");
+        } else {
+            E element = top.getElement();
+            top = top.getNext();
+            --size;
         }
+        return element;
+    }
+    /**
+     * <h2>top()</h2>
+     * <p>
+     * Método que devuelve el elemento que está en la cima de la pila. Se comprueba si la pila está vacía, si lo está, se lanza una excepción de tipo EmptyStackException.
+     * </p>
+     * @return Elemento que esta en la cima de la pila
+     */
+    public E top() throws EmptyStackException {
+        if (isEmpty()) {
+            throw new EmptyStackException("La pila está vacía");
+        } else {
+            return top.getElement();
+        }
+    }
+	/**
+    * <h2>size()</h2>
+    * <p>
+    * Método que devuelve el número de elementos que existe en la pila.
+    * </p>
+    * @return tamaño de la pila
+    */
+   @Override
+	public int size() {
+        return size;
+    }
+    /**
+	* <h2>isEmpty()</h2>
+    * <p>
+    * Método que comprueba si la pila está vacía. Se comprueba si el top es null.
+    * </p>
+    * @return true si la pila está vacía, false en caso contrario.
+    */
+   @Override
 	public boolean isEmpty() {
-		/**
-		 * Se inicializa la pila estando vacia y hacer un pop actualizamos con top.next para ver el siguiente elemento
-		 * Al hacer un push (Node<E> newNode = new Node<>(element);
-         * newNode.next = top;
-         * top = newNode;
-         * size++;)
-		 */
 		return top == null ;
 	}
 }
