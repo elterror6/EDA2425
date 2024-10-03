17e1c360b02f51bb9dd7aa0d34581917bff40a4d
Daniel <danibasketball2000@gmail.com>
2024-10-03 14:17:49 +0200

    COMMIT: Operaciones push(), pop() y top() de la StaticStack.

--- Stack/src/pila/StaticStack.java
+++ Stack/src/pila/StaticStack.java
@@ -1,13 +1,31 @@
 package pila;
 
 public class StaticStack<E> implements Stack<E> {
-	private int topIndex;
-	 @Override
-	    public boolean isEmpty() {
-	        return topIndex == -1;
-	    }
-	 @Override
-	    public int size() {
-	        return topIndex + 1;
-	    }
+	private E[] stack;
+
+	public StaticStack() {
+		this.stack = (E[]) new Object[1];
+	}
+	@Override
+	public void push(E element) {
+
+	}
+	@Override
+	public E pop() {
+		E e;
+		return e;
+	}
+	@Override
+	public E top() {
+		E e;
+		return e;
+	}
+	@Override
+	public boolean isEmpty() {
+        return topIndex == -1;
+	}
+	@Override
+	public int size() {
+	    return topIndex + 1;
+	}
 }
