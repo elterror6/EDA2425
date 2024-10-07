89f4337e882f71c2ad5796474ce7979df377a212
elterror6 <danibasketball2000@gmail.com>
2024-10-07 20:37:28 +0200

    Actualización del main para probar las operaciones toString().

--- Stack/Stack/src/Client/Main.java
+++ Stack/Stack/src/Client/Main.java
@@ -8,7 +8,7 @@ public class Main {
 	     for (int i = 0; i < 10; i++) {
 	         dstack.push(i);
 	     }
-
+	     System.out.println(dstack.toString());
 	     while (dstack.isEmpty() == false) { 
 	         System.out.print(dstack.pop() + " "  );
 	     }
