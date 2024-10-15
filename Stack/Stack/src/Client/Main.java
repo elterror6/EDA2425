26bec584a8291c0150bb511ddf56b53c212b662a
elterror6 <danibasketball2000@gmail.com>
2024-10-15 23:43:54 +0200

    Actualización de la documentación del main y poder insertar una cadena por teclado

--- Stack/Stack/src/Client/Main.java
+++ Stack/Stack/src/Client/Main.java
@@ -1,23 +1,51 @@
 package Client;
 import pilasPackage.*;
+import java.util.Scanner;
+/**
+ * <p>
+ * Clase para probar la estructura de pila con un ejercicio de palindromo.
+ * </p>
+ */
 public class Main {
-
+	/**
+	 * <p>
+	 * Metodo main para realizar la prueba del metodo del palindromo.<br>Comprueba si la cadena que se ha pasado por teclado es un palindromo o no.
+	 * </p>
+	 * @param args Argumentos adicionales
+	 */
 	public static void main(String[] args) {
+		String s;
 		Stack<Character> sstack = new StaticStack<>();
 		Stack<Character> dstack = new DinamicStack<>();
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Introduzca una frase o palabra: ");
+		s=sc.nextLine();
+		
+		
 		System.out.println("Pila dinamica: ");
-		if (Palindromo("Amigo no gima",dstack)) {
+		if (Palindromo(s,dstack)) {
 			System.out.println("La cadena es un palindromo.");
 		} else {
 			System.out.println("La cadena no es un palindromo.");
 		}
 		System.out.println("Pila estatica: ");
-		if (Palindromo("Amigo no gima",sstack)) {
+		if (Palindromo(s,sstack)) {
 			System.out.println("La cadena es un palindromo.");
 		} else {
 			System.out.println("La cadena no es un palindromo.");
 		}
 	}
+	/**
+	 * <p>
+	 * Metodo para ver si una cadena dada es un palindromo o no introduciendose una pila para facilitar la operacion y probar las clases DinamicStack y StaticStack.
+	 * </p>
+	 * @param <E> Elemento generico
+	 * @param s Cadena de caracteres que puede ser un palindromo
+	 * @param pila Pila para realizar la comprobacion del palindromo
+	 * @return True si la cadena de caracteres dada es un palindromo o False si no lo es
+	 */
 	public static <E> boolean Palindromo(String s,Stack<E> pila) {
 		Character c;
 		boolean result = true;
@@ -33,10 +61,5 @@ public class Main {
 		}
 		return result;
 	}
-<<<<<<< Updated upstream
-=======
-	
-	
 
->>>>>>> Stashed changes
 }
