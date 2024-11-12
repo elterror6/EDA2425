4acf25a655d78420226aa1a02ea32c35c587e029
DeigoRh <diegorepulloh@gmail.com>
2024-11-12 17:33:00 +0100

    Add files via upload

---
+++ Actividad3/src/Classes/Termino.java
@@ -0,0 +1,18 @@
+package actividad3;
+
+public class Termino {
+private String termino;
+private String definicion;
+
+public Termino(String termino,String definicion) {
+	this.termino = termino;
+	this.definicion = definicion;
+}
+public String getTermino() {
+	return termino;
+}
+public String getDefinicion() {
+	return definicion;
+}
+
+}
