d8c67b042f3e93d251bcbed2a3ba4355f0e6bca7
Daniel <danibasketball2000@gmail.com>
2024-11-12 18:06:47 +0100

    Adición de Javadoc.

--- Actividad3/src/Classes/Termino.java
+++ Actividad3/src/Classes/Termino.java
@@ -1,18 +1,44 @@
-package actividad3;
+package Classes;
 
+// TODO: Auto-generated Javadoc
+/**
+ * The Class Termino.
+ */
 public class Termino {
-private String termino;
-private String definicion;
+	
+	/** The termino. */
+	private String termino;
+	
+	/** The definicion. */
+	private String definicion;
 
-public Termino(String termino,String definicion) {
-	this.termino = termino;
-	this.definicion = definicion;
-}
-public String getTermino() {
-	return termino;
-}
-public String getDefinicion() {
-	return definicion;
-}
+	/**
+	 * Instantiates a new termino.
+	 *
+	 * @param termino the termino
+	 * @param definicion the definicion
+	 */
+	public Termino(String termino,String definicion) {
+		this.termino = termino;
+		this.definicion = definicion;
+	}
+	
+	/**
+	 * Gets the termino.
+	 *
+	 * @return the termino
+	 */
+	public String getTermino() {
+		return termino;
+	}
+	
+	/**
+	 * Gets the definicion.
+	 *
+	 * @return the definicion
+	 */
+	public String getDefinicion() {
+		return definicion;
+	}
 
 }
