3f9800eaa32a260f3bc0f7502e662d25f4303f5f
Daniel <danibasketball2000@gmail.com>
2024-12-03 17:24:59 +0100

    Adición de javadoc para la documentación de las clases Weight y DecoratedElement

--- ActividadGOT/src/Classes/DecoratedElement.java
+++ ActividadGOT/src/Classes/DecoratedElement.java
@@ -2,13 +2,36 @@ package Classes;
 
 import graphsDSESIUCLM.Element;
 
+/**
+ * La Clase DecoratedElement. Representa un elemento decorado.
+ * @author Daniel Díaz García
+ * @author Diego Repullo Higueruela
+ *
+ * @param <E> El tipo de elemento que se quiere decorar.
+ */
 public class DecoratedElement<E> implements Element {
+	
+	/** El id. */
 	private String ID;
+	
+	/** El elemento. */
 	private E element;
+	
+	/** Elemento visitado. */
 	private boolean visited;
+	
+	/** El elemento padre. */
 	private DecoratedElement<E> parent;
+	
+	/** La distancia. */
 	private int distance;
 	
+	/**
+	 * Instancia a nuevo elemento decorado.
+	 *
+	 * @param ID El ID del elemento
+	 * @param element El elemento que se quiere decorar
+	 */
 	public DecoratedElement (String ID, E element) {
 		this.element=element;
 		this.ID = ID;
@@ -17,37 +40,103 @@ public class DecoratedElement<E> implements Element {
 		this.distance = 0;
 	}
 	
+	/**
+	 * Consulta el elemento que se ha decorado.
+	 *
+	 * @return el elemento que se ha decorado.
+	 */
 	public E getElement() {
 	    return element;
 	}
+	
+	/**
+	 * Consulta si el elemento de ha visitado.
+	 *
+	 * @return true si el elemento se ha visitado
+	 */
 	public boolean getVisited() {
 	  return visited;
 	}
+	
+	/**
+	 * Cambia a visitado el elemento.
+	 *
+	 * @param t se visita el elemento
+	 */
 	public void setVisited(boolean t) {
 	  visited = t;
 	}
+	
+	/**
+	 * Consulta el padre del elemento decorado.
+	 *
+	 * @return el padre del elemento decorado
+	 */
 	public DecoratedElement<E> getParent() {
 	  return parent;
 	}
+	
+	/**
+	 * Cambia el padre del elemento decorado.
+	 *
+	 * @param u El nuevo padre del elemento decorado
+	 */
 	public void setParent(DecoratedElement<E> u) {
 	  parent = u;
 	}
+	
+	/**
+	 * Consulta la distancia.
+	 *
+	 * @return la distancia
+	 */
 	public int getDistance() {
 	  return distance;
 	}
+	
+	/**
+	 * Cambiar la distancia.
+	 *
+	 * @param d la nueva distancia
+	 */
 	public void setDistance(int d) {
 	  distance = d;
 	}
+	
+	/**
+	 * Método que comprueba si un elemento dado es igual a este mismo elemento.
+	 *
+	 * @param n El elemento que se quiere comprobar
+	 * @return true, si son iguales
+	 */
 	public boolean equals (Object n) {
 		return (ID.equals(((DecoratedElement) n).getID()) && element.equals(((DecoratedElement<E>) n).getElement()));
 	}
+	
+	/**
+	 * Hash code.
+	 *
+	 * @return El entero del elemento
+	 */
 	public int hashCode () {
 		return this.element.hashCode();
 	}
+	
+	/**
+	 * A string.
+	 *
+	 * @return La string del elemento
+	 */
 	@Override
 	public String toString() {
 		return element.toString();   
 	}
+	
+	/**
+	 * Consulta el id del elemento decorado.
+	 *
+	 * @return el id del elemento decorado
+	 */
 	@Override
 	public String getID() {
 		return this.ID;
--- ActividadGOT/src/Classes/Weight.java
+++ ActividadGOT/src/Classes/Weight.java
@@ -2,10 +2,30 @@ package Classes;
 
 import graphsDSESIUCLM.Element;
 
+// TODO: Auto-generated Javadoc
+/**
+ * La Clase Weight. Representa una arista con un peso entero.
+ *
+ * @param <E> El tipo de elemento de los vértices que une la arista. Es requerido para generar el ID.
+ */
 public class Weight <E> implements Element {
+	
+	/** El peso. */
 	private int weight;
+	
+	/** Los vértices que une la arista. */
 	private DecoratedElement<E> source, destination;
+	
+	/** El id de la arista. */
 	private String ID;
+	
+	/**
+	 * Instancia un nuevo peso.
+	 *
+	 * @param source El primer vértice de unión de la arista
+	 * @param destination El segundo vértice de unión de la arista
+	 * @param weight El peso
+	 */
 	public Weight (DecoratedElement<E> source,DecoratedElement<E> destination, int weight) {
 		this.source = source;
 		this.destination = destination;
@@ -13,17 +33,33 @@ public class Weight <E> implements Element {
 		this.ID = this.toString();
 	}
 	
+	/**
+	 * Consulta el peso de la arista.
+	 *
+	 * @return el peso de la arista.
+	 */
 	public int getWeight () {
 		return this.weight;
 	}
-	public String toString (String source, String destination) {
-		return source.hashCode() + destination.hashCode()+"";
-	}
+	
+	/**
+	 * A string. Este método se utiliza para generar el ID de esta clase.
+	 *
+	 * @param source El primer vértice de unión de la arista
+	 * @param destination El segundo vértice de unión de la arista
+	 * @return La cadena de texto generada de la suma de los códigos hash de los parámetros dados.
+	 */
 	@Override
 	public String toString () {
 		return this.source.hashCode() + this.destination.hashCode()+"";
 		
 	}
+	
+	/**
+	 * Consulta el id de la arista.
+	 *
+	 * @return el id de la arista
+	 */
 	@Override
 	public String getID() {
 		return this.ID;
